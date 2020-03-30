80bfd87a34e098355cb4a55819ddd3c156a660b7
Ben Jasso <benjaminjasso@icloud.com>
2020-03-30 15:40:57 -0500

    initial commit with add tests

---
+++ CustomListClassTests/CustomListClassTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProject;
+
+namespace CustomListClassTests
+{
+    [TestClass]
+    public class CustomListClassTests
+    {
+        [TestMethod]//1
+        public void Add_CheckCount_AddNegativeNumber()
+        {
+           // arrange
+            
+            ListB<int> listB = new ListB<int>();
+            int value = -3;
+            int expected = 1;
+            int actual;
+
+            //act
+            listB.Add(value);
+            actual = listB.Count;
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]//2
+        public void Add_CheckCount_AddPositiveNumber()
+        {
+            // arrange
+
+            ListB<int> listB = new ListB<int>();
+            int value = 23;
+            int expected = 1;
+            int actual;
+
+            //act
+            listB.Add(value);
+            actual = listB.Count;
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]//3
+        public void Add_CheckIfNewArrayIsMade_Add5thElement()
+        {
+            // arrange
+
+            ListB<int> listB = new ListB<int>();
+            int value = 23;
+            int expected = 8;
+            int actual;
+
+            //act
+            listB.Add(value);
+            listB.Add(value + 10);
+            listB.Add(value + 20);
+            listB.Add(value + 15);
+            listB.Add(value + 32);
+            actual = listB.Capacity;
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]//4
+        public void Add_CheckListCount_Add5thElement()
+        {
+            // arrange
+
+            ListB<int> listB = new ListB<int>();
+            int value = 23;
+            int expected = 5;
+            int actual;
+
+            //act
+            listB.Add(value);
+            listB.Add(value + 10);
+            listB.Add(value + 20);
+            listB.Add(value + 15);
+            listB.Add(value + 32);
+            actual = listB.Count;
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]//5
+        public void Add_CheckArrayOrderStayedSame_Add5thElement()
+        {
+            // arrange
+
+            ListB<int> listB = new ListB<int>();
+            int value = 23;
+            int expected = value + 20;
+            int actual;
+
+            //act
+            listB.Add(value);
+            listB.Add(value + 10);
+            listB.Add(value + 20);
+            listB.Add(value + 15);
+            listB.Add(value + 32);
+            actual = listB.ElementAt(2);
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CustomListClassTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListClassTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListClassTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac3666f7-fec8-483b-8f10-22b046f69f5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
